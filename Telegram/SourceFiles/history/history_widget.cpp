274842f2d777f81fa7cf6b2208f3eb87011144e5
RadRussianRus <radrussianrus@gmail.com>
2022-08-30 14:24:47 +0300

    [Option][GUI] Profile pic rounding

--- Telegram/SourceFiles/history/history_widget.cpp
+++ Telegram/SourceFiles/history/history_widget.cpp
@@ -2645,7 +2645,7 @@ void HistoryWidget::refreshSendAsToggle() {
 	} else if (_sendAs) {
 		return;
 	}
-	_sendAs.create(this, st::sendAsButton);
+	_sendAs.create(this, st::sendAsButton, ::Kotato::JsonSettings::GetInt("userpic_corner_type"));
 	Ui::SetupSendAsButton(_sendAs.data(), controller());
 }
 
@@ -6624,7 +6624,8 @@ void HistoryWidget::setupGroupCallBar() {
 		HistoryView::GroupCallBarContentByPeer(
 			peer,
 			st::historyGroupCallUserpics.size),
-		Core::App().appDeactivatedValue());
+		Core::App().appDeactivatedValue(),
+		::Kotato::JsonSettings::GetInt("userpic_corner_type"));
 
 	controller()->adaptive().oneColumnValue(
 	) | rpl::start_with_next([=](bool one) {
@@ -6675,7 +6676,8 @@ void HistoryWidget::setupRequestsBar() {
 		this,
 		HistoryView::RequestsBarContentByPeer(
 			peer,
-			st::historyRequestsUserpics.size));
+			st::historyRequestsUserpics.size),
+		::Kotato::JsonSettings::GetInt("userpic_corner_type"));
 
 	controller()->adaptive().oneColumnValue(
 	) | rpl::start_with_next([=](bool one) {
