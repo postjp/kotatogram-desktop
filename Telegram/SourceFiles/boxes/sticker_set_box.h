c14dd9322a6583008f0ff31dd2afb6bcb7861ec2
RadRussianRus <radrussianrus@gmail.com>
2022-09-10 20:37:38 +0300

    [Improvement] Use dropdown menu instead of popup menu in sticker set

--- Telegram/SourceFiles/boxes/sticker_set_box.h
+++ Telegram/SourceFiles/boxes/sticker_set_box.h
@@ -17,6 +17,7 @@ class SessionController;
 
 namespace Ui {
 class PlainShadow;
+class DropdownMenu;
 } // namespace Ui
 
 class StickerSetBox final : public Ui::BoxContent {
@@ -42,12 +43,14 @@ private:
 
 	void updateTitleAndButtons();
 	void updateButtons();
+	bool showMenu(not_null<Ui::IconButton*> button);
 	void addStickers();
 	void copyStickersLink();
 	void handleError(Error error);
 
 	const not_null<Window::SessionController*> _controller;
 	const StickerSetIdentifier _set;
+	base::unique_qptr<Ui::DropdownMenu> _menu;
 
 	class Inner;
 	QPointer<Inner> _inner;
