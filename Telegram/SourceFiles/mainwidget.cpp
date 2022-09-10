29e8200dd73e18277a6a189717bc8660d0a95c6d
RadRussianRus <radrussianrus@gmail.com>
2022-09-11 02:32:20 +0300

    [Improvement] Mention user from context menu

--- Telegram/SourceFiles/mainwidget.cpp
+++ Telegram/SourceFiles/mainwidget.cpp
@@ -2625,6 +2625,13 @@ void MainWidget::searchInChat(Dialogs::Key chat) {
 		_dialogs->setInnerFocus();
 	}
 }
+void MainWidget::mentionUser(PeerData *peer) {
+	if (!_history->peer() || !_history->peer()->canWrite()) {
+		return;
+	}
+
+	_history->mentionUser(peer);
+}
 
 bool MainWidget::contentOverlapped(const QRect &globalRect) {
 	return _history->contentOverlapped(globalRect)
