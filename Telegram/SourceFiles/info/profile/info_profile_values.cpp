7887c64856b0cfd35f2a7aca22bd8fc84d8e0294
RadRussianRus <radrussianrus@gmail.com>
2022-09-11 05:16:24 +0300

    [Improvement] Do not strip links in bio

--- Telegram/SourceFiles/info/profile/info_profile_values.cpp
+++ Telegram/SourceFiles/info/profile/info_profile_values.cpp
@@ -55,6 +55,7 @@ auto PlainUsernameValue(not_null<PeerData*> peer) {
 	});
 }
 
+/*
 void StripExternalLinks(TextWithEntities &text) {
 	const auto local = [](const QString &url) {
 		return !UrlRequiresConfirmation(QUrl::fromUserInput(url));
@@ -72,6 +73,7 @@ void StripExternalLinks(TextWithEntities &text) {
 		ranges::remove_if(text.entities, notLocal),
 		text.entities.end());
 }
+*/
 
 } // namespace
 
@@ -157,14 +159,18 @@ TextWithEntities AboutWithEntities(
 	} else if (isBot) {
 		flags |= TextParseHashtags | TextParseBotCommands;
 	}
+	/*
 	const auto stripExternal = peer->isChat()
 		|| peer->isMegagroup()
 		|| (user && !isBot);
+	*/
 	auto result = TextWithEntities{ value };
 	TextUtilities::ParseEntities(result, flags);
+	/*
 	if (stripExternal) {
 		StripExternalLinks(result);
 	}
+	*/
 	return result;
 }
 
