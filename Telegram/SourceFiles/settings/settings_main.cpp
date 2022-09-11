c9b5b88bce3c60cb2b2ed41a80214d25f15e013d
RadRussianRus <radrussianrus@gmail.com>
2022-09-11 05:26:35 +0300

    [Improvement] Always show folders in settings

--- Telegram/SourceFiles/settings/settings_main.cpp
+++ Telegram/SourceFiles/settings/settings_main.cpp
@@ -281,7 +281,7 @@ void SetupSections(
 	const auto preload = [=] {
 		controller->session().data().chatsFilters().requestSuggested();
 	};
-	const auto account = &controller->session().account();
+	// const auto account = &controller->session().account();
 	const auto slided = container->add(
 		object_ptr<Ui::SlideWrap<Ui::SettingsButton>>(
 			container,
@@ -291,10 +291,13 @@ void SetupSections(
 				st::settingsButton,
 				{ &st::settingsIconFolders, kIconDarkBlue }))
 	)->setDuration(0);
+	/*
 	if (!controller->session().data().chatsFilters().list().empty()
 		|| controller->session().settings().dialogsFiltersEnabled()) {
+	*/
 		slided->show(anim::type::instant);
 		preload();
+	/*
 	} else {
 		const auto enabled = [=] {
 			const auto result = account->appConfig().get<bool>(
@@ -317,6 +320,7 @@ void SetupSections(
 				enabled
 			) | rpl::before_next(preloadIfEnabled));
 	}
+	*/
 	slided->entity()->setClickedCallback([=] {
 		showOther(Folders::Id());
 	});
